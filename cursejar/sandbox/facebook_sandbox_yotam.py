7c94479e8e15c12e7470902b48dfbaa3d99ac55c
almondg <almond.sandbox@gmail.com>
2014-02-06 15:32:12 +0200

    Add sandbox files to try facebook api.

--- cursejar/sandbox/facebook_sandbox_yotam.py
+++ cursejar/sandbox/facebook_sandbox_yotam.py
@@ -1,12 +1,13 @@
 __author__ = 'yotam'
+__author__ = 'shaked'
 # coding: utf-8
 
 import facebook
-import os
 from pprint import pprint
+import locale
 
 graph = facebook.GraphAPI()
-graph.access_token = facebook.get_app_access_token(os.environ['FACEBOOK_APP_ID'], os.environ['FACEBOOK_SECRET_KEY'])
+graph.access_token = facebook.get_app_access_token(locale.FACEBOOK_APP_ID, locale.FACEBOOK_SECRET_KEY)
 
 results = graph.get_connections('715900751', 'friends')
 pprint(results)
