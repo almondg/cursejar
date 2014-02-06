bc045b499e4758bc5e1176a1c3542718b846c1ed
almondg <almond.sandbox@gmail.com>
2014-02-06 13:00:06 +0200

    Add sandbox files to try facebook api.

---
+++ cursejar/sandbox/facebook_sandbox.py
@@ -0,0 +1,7 @@
+__author__ = 'shaked'
+
+import django_facebook as facebook
+
+FACEBOOK_TEST_TOKEN = "CAAEXHZAZCOxjkBACyHUY0q0wbEoIWPEpWt3TUPBpL1dEj9qav9BZB9rcyF5vffTx8lT9hB7wASxgPDHZBg6E7cZAsMYQxikN4SsTpWrYSOq5sTbFLqB24MSZA9TteKvVH8doeBl1MxaZCSQ22e1ZAMR0NsrM8glFpwlOJGm65V0fbbJneDl54y6bDUmRg7evnV4ZD"
+
+graph = facebook
