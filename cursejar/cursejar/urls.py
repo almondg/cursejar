3a0f0ceba76c7d25627a891a7c6eb09c046ad635
almondg <almond.sandbox@gmail.com>
2014-02-07 12:57:31 +0200

    fix migrations

--- cursejar/cursejar/urls.py
+++ cursejar/cursejar/urls.py
@@ -5,14 +5,15 @@ from core import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
     url(r'^challenge/(?P<pk>\d+)/$', views.ChallengeView.as_view(),
         name='challenge-details', ),
     url(r'^user/(?P<pk>\d+)/$', views.PersonView.as_view(),
         name='user-details'),
-    url(r'^facebook-view/(?P<pk>\d+)/$', views.FacebookView.as_view(),
-        name='facebook-view'),
+    # url(r'^facebook-view/(?P<pk>\d+)/$', views.FacebookView.as_view(),
+    #     name='facebook-view'),
     url(r'^create-a-challenge/$', views.CreateChallenge.as_view(),
         name='create-a-challenge'),
     url(r'^view-all-challenges/(?P<pk>\d+)/$', views.AllChallenges.as_view(),
