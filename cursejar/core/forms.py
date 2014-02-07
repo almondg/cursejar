6bd4ac9021fedf590c0bc27b05da0b92565c2b7a
almondg <almond.sandbox@gmail.com>
2014-02-07 05:02:12 +0200

    Add tmp for yotam

---
+++ cursejar/core/forms.py
@@ -0,0 +1,61 @@
+from allauth import models
+from django.shortcuts import render
+from djangotoolbox.fields import ListField
+
+__author__ = 'shaked'
+
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Challenge, Word
+
+
+class ChallengeForm(forms.ModelForm):
+    class Meta:
+        model = Challenge
+
+
+class WordForm(forms.ModelForm):
+    class Meta:
+        model = Word
+
+ChallengeFormSet = inlineformset_factory(Challenge, Word)
+
+
+class StringListField(forms.CharField):
+    def prepare_value(self, value):
+        return ', '.join(value)
+
+    def to_python(self, value):
+        if not value:
+            return []
+        return [item.strip() for item in value.split(',')]
+
+
+class CategoryField(ListField):
+    def formfield(self, **kwargs):
+        return models.Field.formfield(self, StringListField, **kwargs)
+
+
+class MyForm(forms.Form):
+    original_field = forms.CharField()
+    extra_field_count = forms.CharField(widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        extra_fields = kwargs.pop('extra', 0)
+
+        super(MyForm, self).__init__(*args, **kwargs)
+        self.fields['extra_field_count'].initial = extra_fields
+
+        for index in range(extra_fields):
+            # generate extra fields in the number specified via extra_fields
+            self.fields['extra_field_{index}'.format(index=index)] = \
+                forms.CharField()
+
+    def myview(request):
+        if request.method == 'POST':
+            form = MyForm(request.POST, extra=request.POST.get('extra_field_count'))
+            if form.is_valid():
+                print "valid!"
+            else:
+                form = MyForm()
+        return render(request, "template", { 'form': form })
--- cursejar/core/views.py
+++ cursejar/core/views.py
@@ -1,8 +1,14 @@
+from allauth import models
 from django.core.urlresolvers import reverse
+from django.forms.models import ModelForm, modelformset_factory
+
 from django.views.generic.detail import DetailView
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.views.generic.edit import CreateView
+from django.forms.formsets import formset_factory
+from djangotoolbox.fields import ListField
+from core.forms import ChallengeForm, ChallengeFormSet, StringListField
 
 from models import Challenge, Person
 
@@ -27,6 +33,30 @@ class AllChallenges(DetailView):
     template_name = 'main/all_challenges.html'
 
 
+class AddChallengeView(CreateView):
+    template_name = 'main/add_challenge.html'
+    form_class = ChallengeForm
+
+    def get_context_data(self, **kwargs):
+        context = super(AddChallengeView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = ChallengeFormSet(self.request.POST)
+        else:
+            context['formset'] = ChallengeFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.object.get_absolute_url())  # assuming your model has ``get_absolute_url`` defined.
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
 class CreateChallenge(CreateView):
     model = Challenge
     fields = ['name', 'end_date', 'participant']
@@ -38,3 +68,5 @@ class CreateChallenge(CreateView):
     def get_success_url(self):
         result = reverse(viewname='challenge-details', current_app='core', kwargs={'pk': self.object.pk})
         return result
+
+
